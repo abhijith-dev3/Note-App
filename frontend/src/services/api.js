60a9c080eecb118094ed03c8660887379e245c31
Abhijith S <abhijith4389@gmail.com>
2026-05-21 07:10:35 -0900

    api updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://note-app-mxiy.onrender.com/api"
 })
 
 API.interceptors.request.use((config) => {
